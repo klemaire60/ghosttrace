721f82ebae2fea8429fe590aa2abcd27187d5dd6
klemaire60 <lemaire.kevin6020@gmail.com>
2026-03-28 17:36:22 +0100

    refactor(cli): implement display for the output

--- ghosttrace/cli.py
+++ ghosttrace/cli.py
@@ -1,5 +1,6 @@
 import click
 from ghosttrace.modules.web import dns_lookup, whois_lookup, http_headers_lookup
+from ghosttrace.output.display import display_dns, display_whois, display_http
 
 @click.group()
 def cli():
@@ -13,10 +14,9 @@ def version():
 @click.argument("domain")
 def web(domain):
 	"""Do a DNS, WHOIS and HTTP Headers scan on the provided domain"""
-	print(f"Scanned domain: {domain}\n")
-	print(f"===== DNS =====\n{dns_lookup(domain)}\n")
-	print(f"===== WHOIS =====\n{whois_lookup(domain)}\n")
-	print(f"===== HTTP HEADERS =====\n{http_headers_lookup(domain)}\n")
+	display_dns(dns_lookup(domain))
+	display_whois(whois_lookup(domain))
+	display_http(http_headers_lookup(domain))
 
 def main():
 	cli()
